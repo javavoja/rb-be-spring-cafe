9f99b86c88343fb6c6ffdc5a5ca51c7e73c457c2
javavoja <javavoja@protonmail.com>
2023-06-27 23:27:30 +0900

    chore: 초기 상태 세팅 1.Gradle 빌드  2.프로젝트 디렉토리 구조 변경 3.리소스 추가  4.스프링 부트 메인 클래스 생성

---
+++ src/test/java/spring/cafe/CafeApplicationTest.java
@@ -0,0 +1,13 @@
+package spring.cafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
